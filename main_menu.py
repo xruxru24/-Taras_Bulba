36169a868795e47416b4d32192b99aba2680b8e5
Admin <arsenijcugajnov709@gmail.com>
2025-01-22 23:34:30 +0300

    add: stats.csv(database with stats of player: kills, damage and others), func statictics() and method update_stats in Engine, load music(song_1, song_2), fix bugs

--- main_menu.py
+++ main_menu.py
@@ -3,15 +3,20 @@ import sys
 import Units
 import InfinityMode
 import expansion
+import csv
 from expansion import SCREEN
 
 pygame.mixer.init()
+image = expansion.load_image("startmenu.png")
+image1 = expansion.load_image("statsmenu.png")
 
 
 class Engine:
     def __init__(self):
         self.kills = 0
         self.damage = 0
+        self.deaths = 0
+        self.hits = 0
 
     def music(self, songs):
         for i in songs:
@@ -31,6 +36,7 @@ class Engine:
                     unit.health = 0
                     creeps.pop(unit)
                     if isinstance(unit, Units.Player):
+                        self.deaths += 1
                         end_game()
                     else:
                         self.kills += 1
@@ -38,6 +44,20 @@ class Engine:
                 else:
                     unit.health -= weapon.damage
                     self.damage += weapon.damage
+                self.hits += 1
+        self.update_stats()
+
+    def update_stats(self):
+        with open('stats.csv', mode='wt') as file:
+            data = [self.damage, self.kills, self.deaths, self.hits]
+            data1 = ['Damage', 'Kills', 'Deaths', 'Hits']
+            temp = csv.writer(file, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+            for i in range(len(data)):
+                temp.writerow([data1[i], data[i]])
+
+
+eng = Engine()
+font = pygame.font.SysFont(None, 50)
 
 
 def company_game():
@@ -45,7 +65,26 @@ def company_game():
 
 
 def statictics():
-    pass
+    data_coords = {'Deaths': (100, 200),
+                   'Kills': (80, 300),
+                   'Hits': (70, 400),
+                   'Damage': (100, 500)}
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and \
+                    event.pos[0] in range(400, 500) and event.pos[1] in range(450, 500):
+                start_menu()
+                run = False
+        expansion.SCREEN.blit(image1, (400, 300))
+        with open('stats.csv', mode='rt') as file:
+            data = list(csv.reader(file, delimiter=';', quotechar='"'))
+            for stat in data:
+                num = font.render(str(stat[0]), False, 'red')
+                expansion.SCREEN.blit(num, (data_coords[stat[0]]))
+        pygame.display.flip()
 
 
 def exit_game():
@@ -54,10 +93,6 @@ def exit_game():
 
 def end_game():
     pass
-
-
-image = expansion.load_image("startmenu.png")
-eng = Engine()
 
 
 def start_menu():
@@ -71,7 +106,8 @@ def start_menu():
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                click_data = {(range(15, 435), range(345, 480)): (InfinityMode.infinity_game, eng.music(('song_2.mp3', 'song_1.mp3'))),
+                click_data = {(range(15, 435), range(345, 480)): (
+                InfinityMode.infinity_game, eng.music(('song_2.mp3', 'song_1.mp3'))),
                               (range(1650, 1920), range(895, 1025)): exit_game,
                               (range(15, 435), range(175, 315)): company_game,
                               (range(15, 435), range(525, 655)): statictics}
@@ -82,7 +118,7 @@ def start_menu():
         expansion.SCREEN.fill('black')
         expansion.SCREEN.blit(image, (0, 0))
         pygame.display.flip()
-        expansion.CLOCK.tick(60)
+        expansion.CLOCK.tick(10)
 
 
 start_menu()
